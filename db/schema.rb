2eb2f9ec19198a3c9c8c72cd75d35692386aaca8
bdarab <bdarabzand@gmail.com>
2020-09-21 23:44:36 +0200

    Add uuid

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_21_170553) do
+ActiveRecord::Schema.define(version: 2020_09_21_213202) do
 
   create_table "purchases", force: :cascade do |t|
     t.string "email"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_09_21_170553) do
     t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uuid"
   end
 
 end
